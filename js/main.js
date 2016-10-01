2dfcb49268608612dbb006ab26d5ad928a29edf5
woodall <dave@woodalls.me>
2016-09-30 22:08:27 -0400

    now I'm just hacking....

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
-  // var welcome = new APP.Partial('welcome.html', 'welcome');
-  // welcome.render();
+  var welcome = new APP.Partial('welcome.html', 'welcome');
+  welcome.render();
 
   var nav = new APP.Nav;
   nav.html();
